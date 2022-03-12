fdd8ab512020d6f578364d9d19add5b2ad4ddec6
adizicher <adikadoche@gmail.com>
2022-03-12 02:26:05 +0200

    fixed matcher bug

--- coref/matcher.py
+++ coref/matcher.py
@@ -56,13 +56,14 @@ class HungarianMatcher(nn.Module):
         if gold_matrix.shape[1] == 0 or torch.sum(gold_matrix) == 0:
             return False, False
 
-        coref_logits = coref_logits[:self.num_queries, :-1] # [num_queries, tokens_without_dummy]
+        new_coref_logits = coref_logits[:self.num_queries, :-1] # [num_queries, tokens_without_dummy]
+        new_coref_logits[:, -1] = new_coref_logits[:, -1] + coref_logits[:self.num_queries, -1]
 
         real_cluster_target_rows = torch.sum(gold_matrix, -1) > 0
         real_cluster_target = gold_matrix[real_cluster_target_rows]
 
-        cluster_repeated = real_cluster_target[:,:-1].unsqueeze(1).repeat(1, coref_logits.shape[0], 1)
-        coref_logits_repeated = coref_logits.unsqueeze(0).repeat(real_cluster_target.shape[0], 1, 1)
+        cluster_repeated = real_cluster_target[:,:-1].unsqueeze(1).repeat(1, new_coref_logits.shape[0], 1)
+        coref_logits_repeated = new_coref_logits.unsqueeze(0).repeat(real_cluster_target.shape[0], 1, 1)
         clamped_logits1 = coref_logits_repeated[:,:,:-1].clamp(max=1.0)
         cost_coref = torch.mean(F.binary_cross_entropy(clamped_logits1, \
             cluster_repeated, reduction='none'), -1) + \
